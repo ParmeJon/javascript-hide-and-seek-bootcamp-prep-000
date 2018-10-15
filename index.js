412e0206ce29470031bda115909f36f7acf8f7a0
Jonathan Chan <jonchan816@gmail.com>
2018-10-15 08:16:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   var startingNode = document.querySelector('div#grand-node');
-  for (let i = 0; isNan(startingNode) ; i++){
+  for (let i = 0; isNaN(startingNode) ; i++){
     startingNode = startingNode[i].innerHTML;
   }
   return startingNode;
