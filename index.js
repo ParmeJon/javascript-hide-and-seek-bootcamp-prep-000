74987b698005ca7271b72cae70dea27a84cdefbf
Jonathan Chan <jonchan816@gmail.com>
2018-10-15 08:28:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  const firstSelection = document.querySelector(selector);
+  return firstSelection;
+}
 
+function nestedTarget() {
+  const targetNested = document.querySelector("#nested .target");
+  return targetNested
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < rankedLists.length; i++ ) {
+    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var initialNode = document.querySelector('#grand-node');
+  var deeperNode = initialNode.querySelector('div');
+  while(deeperNode) {
+    initialNode = deeperNode;
+    deeperNode = initialNode.querySelector('div');
+  }
+  return initialNode;
+}
