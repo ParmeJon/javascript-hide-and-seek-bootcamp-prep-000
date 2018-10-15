3fbf3979ba6a3e2a285f09d637d2c044c49b1b46
Jonathan Chan <jonchan816@gmail.com>
2018-10-15 07:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var rankedLists = document.querySelectorAll('ul.ranked-list li');
+  const rankedLists = document.querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < rankedLists.length; i++ ) {
     rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n;
   }
